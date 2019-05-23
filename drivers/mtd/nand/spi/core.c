5a661008ca833f9a7b4cdb0017b80902f6a432fb
Dmitry Konyshev <probables@emcraft.com>
2019-05-23 22:03:28 +0300

    RM3446 Remove unused legacy API that caused compilation warnings in drivers/mtd/nand/spi/core.c

--- drivers/mtd/nand/spi/core.c
+++ drivers/mtd/nand/spi/core.c
@@ -1285,18 +1285,10 @@ static int spi_flash_std_erase(struct udevice *dev, u32 offset, size_t len)
 	return mtd->_erase(mtd, &instr);
 }
 
-static int spi_flash_std_get_sw_write_prot(struct udevice *dev)
-{
-	struct spi_flash *flash = dev_get_uclass_priv(dev);
-
-	return spi_flash_cmd_get_sw_write_prot(flash);
-}
-
 static const struct dm_spi_flash_ops spi_flash_std_ops = {
 	.read = spi_flash_std_read,
 	.write = spi_flash_std_write,
 	.erase = spi_flash_std_erase,
-	.get_sw_write_prot = spi_flash_std_get_sw_write_prot,
 };
 
 U_BOOT_DRIVER(spinand) = {
